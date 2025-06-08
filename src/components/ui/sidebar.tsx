b2fb866483a3f97e73294844959735190f75222e
Ashik <ashekur.rahman@goama.com>
2025-06-08 17:03:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -279,8 +279,8 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
-      <span className="sr-only">Toggle Sidebar</span>
+      <div><PanelLeft />
+        <span className="sr-only">Toggle Sidebar</span></div>
     </Button>
   )
 })
@@ -619,7 +619,7 @@ const SidebarMenuAction = React.forwardRef<
         "peer-data-[size=lg]/menu-button:top-2.5",
         "group-data-[collapsible=icon]:hidden",
         showOnHover &&
-          "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
+        "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
         className
       )}
       {...props}
